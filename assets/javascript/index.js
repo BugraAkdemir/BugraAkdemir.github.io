c5b28d0f90c7d212ddd966326893c70f870521b8
Bugra <bugrakaptan5@gmail.com>
2024-07-05 02:36:32 +0300

    7.1 Ads

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -4,23 +4,3 @@ function changeTheme() {
   }
 
 
-  document.addEventListener('DOMContentLoaded', (event) => {
-    // Tarayıcıda depolanan sayacı al
-    let counter = localStorage.getItem('page_view_count');
-
-    // Sayı yoksa sıfırla başlat
-    if (!counter) {
-        counter = 0;
-    } else {
-        counter = parseInt(counter);
-    }
-
-    // Sayacı bir artır
-    counter++;
-
-    // Yeni değeri depola
-    localStorage.setItem('page_view_count', counter);
-
-    // Sayacı göster
-    document.getElementById('counter').innerText = counter;
-});
