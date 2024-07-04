27710d8af2dd20d150d4a02cacbb71f0ac72fb34
Bugra <bugrakaptan5@gmail.com>
2024-07-05 02:27:56 +0300

    7.0 (Beta Test)

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -2,3 +2,25 @@ function changeTheme() {
     let themeVal = document.querySelector('input[name="theme"]').checked ? 'dark' : 'light';
     document.documentElement.setAttribute('data-theme', themeVal);
   }
+
+
+  document.addEventListener('DOMContentLoaded', (event) => {
+    // Tarayıcıda depolanan sayacı al
+    let counter = localStorage.getItem('page_view_count');
+
+    // Sayı yoksa sıfırla başlat
+    if (!counter) {
+        counter = 0;
+    } else {
+        counter = parseInt(counter);
+    }
+
+    // Sayacı bir artır
+    counter++;
+
+    // Yeni değeri depola
+    localStorage.setItem('page_view_count', counter);
+
+    // Sayacı göster
+    document.getElementById('counter').innerText = counter;
+});
