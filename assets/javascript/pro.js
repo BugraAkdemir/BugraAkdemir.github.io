6e571f974c23ccfae46b743ec403595eb4148210
Bugra <bugrakaptan5@gmail.com>
2024-07-17 00:28:45 +0300

    v15.0.0 Demo

--- assets/javascript/pro.js
+++
@@ -1,7 +0,0 @@
-var names = "Leolin";
-var kn = "Creates an Instagram phishing link, spreads this link to the internet via tunnel using 'Ngrok' and starts phishing."
-document.getElementById("lpgr").innerText=kn;
-document.getElementById("lname").innerText=names;
-document.getElementById("llink").addEventListener("click", function() {
-    window.location.href = "https://yeni-link.fffcom";
-  });
--- turkish/assets/php/servers.php
+++
@@ -1,5 +0,0 @@
-<?php
-   echo "Comming Soon 7.0.0";
-
-   echo "Introducing version 7.0.0"
-?>
